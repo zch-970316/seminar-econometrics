9a1c037a40a3f9edefc93f0bacbe9d41a4d7a7d4
huhuaping <huhuaping01@nwsuaf.edu.cn>
2022-06-19 18:45:03 +0800

    rebuild binned mean

--- Rscript/hensen21-fig19-1a.R
+++ Rscript/hensen21-fig19-1a.R
@@ -1,6 +1,7 @@
 #  This file generates Figure 19.1a
 #  Nadaraya-Watson Regression
 
+#### prepare ####
 n <- 100
 xm <- 10
 
@@ -11,10 +12,14 @@ a <- pi/4
 m <- sin((xdat-2)*a)/((xdat-2)*a)
 ydat <- m + rnorm(n)/4
 
+dt <- tibble(index = 1:length(xdat), X = xdat, Y = ydat)
+
+
 # set h
 h1 <- 1          # for binned
 h2 <- h1/sqrt(3) # for NW
 
+#### calculate ####
 x0 <- seq(1,9,2) # bins break
 x0n <- length(x0)
 # index of Logic value in each bins
@@ -26,18 +31,6 @@ m0 <- (k0%*%ydat)/rowSums(k0)
 x <- seq(0,xm,.01)
 xn <- length(x)      # 1001
 
-# repeat binned means for each bins
-x1 <- seq(0,2,.01)            # the first bins 
-hist1 <- matrix(m0[1],201,1)
-x2 <- seq(2,4,.01)
-hist2 <- matrix(m0[2],201,1)
-x3 <- seq(4,6,.01)
-hist3 <- matrix(m0[3],201,1)
-x4 <- seq(6,8,.01)
-hist4 <- matrix(m0[4],201,1)
-x5 <- seq(8,10,.01)
-hist5 <- matrix(m0[5],201,1)
-
 # for Rolling, dim(k1)=c(1001, 100)
 k1 <- (abs(x%*%matrix(1,1,n)-matrix(1,xn,1)%*%t(xdat)) <= h1)
 m1 <- (k1%*%ydat)/rowSums(k1)
@@ -46,42 +39,154 @@ m1 <- (k1%*%ydat)/rowSums(k1)
 k2 <- dnorm(abs(x%*%matrix(1,1,n)-matrix(1,xn,1)%*%t(xdat))/h2)
 m2 <- (k2%*%ydat)/rowSums(k2)
 
-wd <- 1.4
+#### combin all result ####
+tbl_result <- tibble(id = 1:length(x),
+                     x = x,
+                     x_upr = x +0.01) %>%
+  mutate(bd = cut_interval(x,length = 2, right=FALSE)) %>%
+  mutate(
+    lwr = as.numeric(str_extract(bd, "(\\d{1})(?=\\,)")),
+    upr = as.numeric(str_extract(bd, "(?<=\\,)(\\d{1,2})")),
+    mid = 0.5*(lwr+ upr)
+    ) %>%
+  mutate(lft = x-h1,
+         rgt = x +h1,
+         bins = str_c('[',lft,',',rgt, ')')) %>%
+  mutate(m0 = rep(m0, times=c(200,200,200,200,201)),
+         m1 = (as.vector(m1)),
+         m2 = as.vector(m2))
 
-pdf("HANSEN19-1a.pdf",family="Helvetica",colormodel='cmyk',width=4.5,height=4.5)
-plot(x,m1,type="l",lty=1,ylab="Dependent Variable",xlab="Regressor",xaxs="i",yaxs="i",xlim=c(0,10),ylim=c(-.75,1.5),xaxt="n",yaxt="n",lwd=wd,bty="n")
-lines(x,m2,lty=5,lwd=wd)
-points(xdat,ydat,pch=1,cex=.7)
-points(x0,m0,pch=15,cex=1)
-lines(x1,hist1,lty=2,lwd=wd)
-lines(x2,hist2,lty=2,lwd=wd)
-lines(x3,hist3,lty=2,lwd=wd)
-lines(x4,hist4,lty=2,lwd=wd)
-lines(x5,hist5,lty=2,lwd=wd)
-abline(v=2,lty=3,lwd=wd)
-abline(v=4,lty=3,lwd=wd)
-abline(v=6,lty=3,lwd=wd)
-abline(v=8,lty=3,lwd=wd)
-axis(side=1,seq(0,10,1),lwd=wd)
-axis(side=2,seq(-1,2,.5),lwd=wd)
-legend("topright",legend=c("Binned Means","Rolling Binned","Nadaraya-Watson"),lty=c(2,1,5),bg="white",lwd=wd,bty="n")
-dev.off()
 
-postscript("HANSEN19-1a.eps",paper="special",width=4.5,height=4.5,horizontal=FALSE,onefile=FALSE,family="Helvetica",colormodel='cmyk')
-plot(x,m1,type="l",lty=1,ylab="Dependent Variable",xlab="Regressor",xaxs="i",yaxs="i",xlim=c(0,10),ylim=c(-.75,1.5),xaxt="n",yaxt="n",lwd=wd,bty="n")
-lines(x,m2,lty=5,lwd=wd)
-points(xdat,ydat,pch=1,cex=.7)
-points(x0,m0,pch=15,cex=1)
-lines(x1,hist1,lty=2,lwd=wd)
-lines(x2,hist2,lty=2,lwd=wd)
-lines(x3,hist3,lty=2,lwd=wd)
-lines(x4,hist4,lty=2,lwd=wd)
-lines(x5,hist5,lty=2,lwd=wd)
-abline(v=2,lty=3,lwd=wd)
-abline(v=4,lty=3,lwd=wd)
-abline(v=6,lty=3,lwd=wd)
-abline(v=8,lty=3,lwd=wd)
-axis(side=1,seq(0,10,1),lwd=wd)
-axis(side=2,seq(-1,2,.5),lwd=wd)
-legend("topright",legend=c("Binned Means","Rolling Binned","Nadaraya-Watson"),lty=c(2,1,5),bg="white",lwd=wd,bty="n")
-dev.off()
+tbl_match <- tbl_result %>%
+  mutate(data = map(x,~dt)) %>%
+  unnest(data) %>%
+  mutate(
+    isbins = ifelse(
+      ((X>=x-h1)& (X<(x+h1))),
+      1, 0)
+  )  %>%
+  mutate(
+    k0=ifelse(
+      ((X>=lwr)& (X<upr)),
+      1, 0),
+    k1=as.numeric(as.vector(t(k1))),
+    k2 = as.vector(t(k2))
+    ) %>%
+  mutate(bd = as.character(bd))
+
+
+# binned mean
+tbl_m0 <- tbl_match %>%
+  filter(k0==1) %>%
+  select(index, X, Y, bd,lwr, upr,mid,k0,m0) %>%
+  unique() %>%
+  arrange(index) %>%
+  group_by(bd) %>%
+  mutate(sum_k = sum(k0),
+         sum_ky = sum(k0*Y)) %>%
+  ungroup() %>%
+  mutate(bins = bd) %>%
+  rename("x"="mid")
+
+# scrolling mean
+tbl_m1 <- tbl_match %>%
+  filter(k1==1) %>%
+  select(index, X, Y, bd,bins,lft, rgt,x,k1,m1) %>%
+  unique() %>%
+  arrange(lft,index) %>%
+  group_by(x) %>%
+  mutate(sum_k = sum(k1),
+         sum_ky = sum(k1*Y)) %>%
+  ungroup() %>%
+  rename("lwr"="lft", "upr"="rgt")
+
+# scrolling mean
+tbl_m2 <- tbl_match %>%
+  #filter(k1==1) %>%
+  select(index, X, Y, bd,bins,lft, rgt,x,k2,m2) %>%
+  unique() %>%
+  arrange(lft,index) %>%
+  group_by(x) %>%
+  mutate(sum_k = sum(k2),
+         sum_ky = sum(k2*Y)) %>%
+  ungroup() %>%
+  rename("lwr"="lft", "upr"="rgt")
+
+
+#### draw plot ####
+
+# basic plot
+p0 <- ggplot() +
+  geom_point(aes(X, Y),data = dt, pch=21) +
+  labs(x= "自变量X", y ="因变量Y") +
+  scale_x_continuous(breaks = seq(0,10,1), limits = c(0,10)) +
+  scale_y_continuous(breaks = seq(-1,2,0.5), limits = c(-1,2)) +
+  theme_bw()
+
+p00 <- p0 +
+  geom_vline(xintercept = x0, lty="dashed") +
+  geom_point(aes(X, Y, color=as.factor(bd)),
+             data = tbl_m0, pch=21) +
+  geom_rect(aes(ymin=-1,ymax=2,
+                xmin= lwr, xmax = upr, 
+                fill = as.factor(bd)),
+            data = tbl_m0 %>% select(bd, lwr, upr) %>% unique,
+            alpha = 0.05, inherit.aes = FALSE) +
+  theme(legend.position = "none")
+
+### binned plot
+p1 <- p00 +
+  geom_point(aes(x, m0),
+             data = tbl_m0 %>% select(x, m0) %>% unique(), 
+             pch=15, color="black") +
+  geom_segment(aes(x =lwr, xend=upr,
+                   y =m0, yend = m0,
+                   color=as.factor(bd)),
+            data = tbl_m0 %>% select(lwr,bd,upr, m0) %>% unique(),
+            lty="solid", lwd=0.8) +
+  theme(legend.position = "none")
+
+## scrolling  plot
+
+p2 <- p0 +
+  geom_point(aes(x, m0),
+             data = tbl_m0 %>% select(x, m0) %>% unique(), 
+             pch=15, color="black") +
+  geom_line(aes(x, m1, color=as.factor(bd)),
+            data = tbl_m1 %>% select(x,m1,bins,bd) %>% unique(), 
+            lty="solid", lwd=0.8) +
+  theme(legend.position = "none")
+
+## Nadaraya-Watson  plot
+p3 <- p0 +
+  geom_point(aes(x, m0),
+             data = tbl_m0 %>% select(x, m0) %>% unique(), 
+             pch=15, color="black") +
+  geom_line(aes(x, m2, color=as.factor(bd)),
+            data = tbl_m2 %>% select(x,m2,bins,bd) %>% unique(), 
+            lty="solid", lwd=0.8) +
+  theme(legend.position = "none")
+
+## all three plot
+
+p_all <- p0 +
+  geom_vline(xintercept = x0, lty="dashed")  +
+  geom_point(aes(x, m0),
+             data = tbl_m0 %>% select(x, m0) %>% unique(), 
+             pch=15, size =3, color="black")  +
+  geom_segment(aes(x =lwr, xend=upr,
+                   y =m0, yend = m0,
+                   color= "binned"),
+               data = tbl_m0 %>% select(lwr,bd,upr, m0) %>% unique(),
+               lty="solid", lwd=0.8)  +
+  geom_line(aes(x, m1, color="rolling"),
+            data = tbl_m1 %>% select(x,m1) %>% unique(), 
+            lty="solid", lwd=0.8) +
+  geom_line(aes(x, m2, color = "NW"),
+            data = tbl_m2 %>% select(x,m2) %>% unique(), 
+            lty="solid", lwd=0.8) +
+  scale_color_manual(
+    name="mx",
+    breaks = c("binned", "rolling","NW"),
+    values=c("green", "red","blue"))+
+  theme(legend.position = "right")
